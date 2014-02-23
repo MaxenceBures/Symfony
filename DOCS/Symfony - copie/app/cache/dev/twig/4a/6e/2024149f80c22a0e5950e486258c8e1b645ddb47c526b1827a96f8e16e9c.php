5875041338d6679e76eed5ce63b7ae3255cb941f
MaxenceBures <maxence.bures@gmail.com>
2014-02-23 23:56:01 +0100

    test

---
+++ DOCS/Symfony - copie/app/cache/dev/twig/4a/6e/2024149f80c22a0e5950e486258c8e1b645ddb47c526b1827a96f8e16e9c.php
@@ -0,0 +1,42 @@
+<?php
+
+/* webStudentEtudiantBundle:Etudiant:index.html.twig */
+class __TwigTemplate_4a6e2024149f80c22a0e5950e486258c8e1b645ddb47c526b1827a96f8e16e9c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        echo "<!DOCTYPE html>
+<html>
+ <head>
+ <title>Bienvenue sur votre première page créée avec Symfony !</title>
+ </head>
+ <body>
+ <h1>Salut les étudiants</h1>
+ <p>
+ Félicitations, vous avez réussi à créer une première page. Vous avez compris ?!?
+ </p>
+ </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "webStudentEtudiantBundle:Etudiant:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 2,);
+    }
+}
---
+++ DOCS/Symfony - copie/app/cache2/dev/twig/02/87/dcdf77fecf9cebd63ce8bd668e592e5fc480ee9e219c2ef93caa251176fa.php
@@ -0,0 +1,51 @@
+<?php
+
+/* webStudentEtudiantBundle:Etudiant:consulter.html.twig */
+class __TwigTemplate_0287dcdf77fecf9cebd63ce8bd668e592e5fc480ee9e219c2ef93caa251176fa extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::layout.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "test
+
+test2
+        ";
+    }
+
+    public function getTemplateName()
+    {
+        return "webStudentEtudiantBundle:Etudiant:consulter.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
