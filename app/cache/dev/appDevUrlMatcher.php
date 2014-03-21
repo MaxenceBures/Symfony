809949013973a0992f3f5686202f4f0ac9527d56
MaxenceBures <maxence.bures@gmail.com>
2014-03-21 16:29:26 +0100

    Modif

--- app/cache/dev/appDevUrlMatcher.php
+++ app/cache/dev/appDevUrlMatcher.php
@@ -271,6 +271,11 @@ class appDevUrlMatcher extends Symfony\Bundle\FrameworkBundle\Routing\Redirectab
             return array (  '_controller' => 'webStudent\\EtudiantBundle\\Controller\\EtudiantController::test3Action',  '_route' => 'test3Action',);
         }
 
+        // Stage_ajouter
+        if ($pathinfo === '/ajouterStage') {
+            return array (  '_controller' => 'webStudent\\EtudiantBundle\\Controller\\EtudiantController::ajouterStageAction',  '_route' => 'Stage_ajouter',);
+        }
+
         // _welcome
         if (rtrim($pathinfo, '/') === '') {
             if (substr($pathinfo, -1) !== '/') {
