07f73a11a3af8bd1f9ad1b0975b81cedfb2142d2
MaxenceBures <maxence.bures@gmail.com>
2014-04-03 11:49:53 +0200

    Avant ManyToOne

---
+++ src/webStudent/EtudiantBundle/Entity/Activite.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace webStudent\EtudiantBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Activite
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="webStudent\EtudiantBundle\Entity\ActiviteRepository")
+ */
+class Activite
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=100)
+     */
+    private $libelle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=5)
+     */
+    private $code;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return Activite
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+     /**
+     * Set code
+     *
+     * @param string $code
+     * @return Activite
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+}
